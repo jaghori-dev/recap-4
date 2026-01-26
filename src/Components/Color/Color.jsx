a96c1fccba6a34edabe94ba82534bb96ced019d5
Enayat Ahmadi <eno.ahmadi22@gmail.com>
2026-01-26 15:17:53 +0100

    add color themes card

--- src/Components/Color/Color.jsx
+++ src/Components/Color/Color.jsx
@@ -1,5 +1,14 @@
 import "./Color.css";
 
-export default function Color({ color }) {
-  return null;
+export default function Color({ color, role, contrast }) {
+  return (
+    <div
+      className="color-card"
+      style={{ backgroundColor: color, color: contrast }}
+    >
+      <h2 className="color-card-headline">{color}</h2>
+      <h3>{role}</h3>
+      <p>Contrast: {contrast}</p>
+    </div>
+  );
 }
