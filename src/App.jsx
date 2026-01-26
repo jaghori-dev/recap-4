3b6820a686b7d8a8c0588d9ec39148f076396d86
Enayat Ahmadi <eno.ahmadi22@gmail.com>
2026-01-26 23:32:30 +0100

    feat: add theme creator form

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import { initialColors } from "./lib/colors";
 import Color from "./Components/Color/Color";
 import "./App.css";
+import ColorForm from "./Components/Color/ColorForm/ColorForm";
+import { useState } from "react";
+import { uid } from "uid";
 
 function App() {
+  const [data, setData] = useState(initialColors);
+  function onSubmitColor(input) {
+    setData([{ id: uid(2), ...input }, ...data]);
+  }
   return (
     <>
-    <h1>Theme Creator</h1>
-      {initialColors.map((color) => {
+      <h1>Theme Creator</h1>
+      <ColorForm onSubmitColor={onSubmitColor} />
+      {data.map((color) => {
         return (
           <Color
             key={color.id}
